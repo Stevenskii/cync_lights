e46c089ab0a935c4594ac529b59c257edb0077c0
Stevenskii <slwbud@gmail.com>
2024-10-08 11:37:39 -0500

    Update __init__.py

--- custom_components/cync_lights/__init__.py
+++ custom_components/cync_lights/__init__.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+
 from .const import DOMAIN
 from .cync_hub import CyncHub
 
@@ -12,20 +13,12 @@ PLATFORMS: list[str] = ["light", "binary_sensor", "switch", "fan"]
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Cync Room Lights from a config entry."""
     hass.data.setdefault(DOMAIN, {})
-    remove_options_update_listener = entry.add_update_listener(options_update_listener)
-    hub = CyncHub(entry.data, entry.options, remove_options_update_listener)
+    hub = CyncHub(entry.data, entry.options)
     hass.data[DOMAIN][entry.entry_id] = hub
     hub.start_tcp_client()
 
-    entry.async_on_update(options_update_listener)
-
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
-
-
-async def options_update_listener(entry: ConfigEntry) -> None:
-    """Handle options update."""
-    await entry.hass.config_entries.async_reload(entry.entry_id)
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
