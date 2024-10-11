57355d69057ba3daf348a134ac7133c95907f169
Stevenskii <slwbud@gmail.com>
2024-10-10 22:51:26 -0500

    Update __init__.py

--- custom_components/cync_lights/__init__.py
+++ custom_components/cync_lights/__init__.py
@@ -17,7 +17,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     hass.data[DOMAIN][entry.entry_id] = hub
     hub.start_tcp_client()
 
-    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    await hass.config_entries.async_setup_platforms(entry, PLATFORMS)
     return True
 
 
